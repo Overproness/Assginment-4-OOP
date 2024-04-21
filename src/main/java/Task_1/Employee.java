f46415e4bba62012073cc04c739019e055e87d36
Overproness <mr.overproness@gmail.com>
2024-04-21 14:14:13 +0500

    Created an idea project and completed Task_1

---
+++ src/main/java/Task_1/Employee.java
@@ -0,0 +1,40 @@
+package Task_1;
+
+public abstract class Employee {
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+    private Date birthDate;
+
+    // constructor
+    public Employee(String firstName, String lastName, String socialSecurityNumber, Date birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s%nsocial security number: %s%nbirth date: %s", getFirstName(), getLastName(),
+                getSocialSecurityNumber(), getBirthDate());
+    }
+
+    public abstract double earnings();
+}
